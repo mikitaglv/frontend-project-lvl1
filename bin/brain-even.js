bb2d9809ba90c5de30af3696a0aab1998e077a39
Nikita Gulyaev <mikita.glv@gmail.com>
2022-12-05 12:06:52 +0300

    Used prettier

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import readlineSync from "readline-sync";
-import greeting from "../src/cli.js";
+import readlineSync from 'readline-sync';
+import greeting from '../src/cli.js';
 
 const isEvenNumber = (number) => number % 2 === 0;
 
@@ -8,24 +8,22 @@ const userName = greeting();
 
 let answers = 0;
 while (answers < 3) {
-    const randomNumber = Math.floor(Math.random() * 100);
-    console.log(`Question: ${randomNumber}`);
-    const userAnswer = readlineSync.question("Your answer: ");
+  const randomNumber = Math.floor(Math.random() * 100);
+  console.log(`Question: ${randomNumber}`);
+  const userAnswer = readlineSync.question('Your answer: ');
 
-    if (userAnswer === "yes" && isEvenNumber(randomNumber)) {
-        console.log("Correct!");
-        answers += 1;
-    } else if (userAnswer === "no" && !isEvenNumber(randomNumber)) {
-        console.log("Correct!");
-        answers += 1;
-    } else {
-        console.log(
-            `'${userAnswer}' is wrong answer :(. Correct answer was 'no'.\nLet's try again, ${userName}!`
-        );
-        break;
-    }
+  if (userAnswer === 'yes' && isEvenNumber(randomNumber)) {
+    console.log('Correct!');
+    answers += 1;
+  } else if (userAnswer === 'no' && !isEvenNumber(randomNumber)) {
+    console.log('Correct!');
+    answers += 1;
+  } else {
+    console.log(`'${userAnswer}' is wrong answer :(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
+    break;
+  }
 
-    if (answers === 3) {
-        console.log(`Congratulations, ${userName}!`);
-    }
+  if (answers === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
 }
