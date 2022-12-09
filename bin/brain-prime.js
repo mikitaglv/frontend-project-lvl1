8d9b6502bf2ab35e8758238e49a203b6ae0fb6f6
Nikita Gulyaev <mikita.glv@gmail.com>
2022-12-09 15:07:26 +0300

    Moved the game logic to a separate module

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,36 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greeting from '../src/cli.js';
+import { gamePrime } from '../src/games/game-prime.js';
 
-const isPrimeNumber = (number) => {
-    if (number < 2) return false;
-    if (number > 1) {
-        for (let i = 2; i < number; i += 1) {
-            if (number % i === 0) return false;
-        };
-    };
-    return true;
-};
-
-const userName = greeting();
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-
-let answers = 0;
-while (answers < 3) {
-  const randomNumber = Math.floor(Math.random() * 100);
-  console.log(`Question: ${randomNumber}`);
-  const userAnswer = readlineSync.question('Your answer: ');
-  const correctAnswer = isPrimeNumber(randomNumber) ? 'yes' : 'no';
-
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-    answers += 1;
-  } else {
-    console.log(`'${userAnswer}' is wrong answer :(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-    break;
-  }
-
-  if (answers === 3) {
-    console.log(`Congratulations, ${userName}!`);
-  }
-}
+gamePrime();
---
+++ src/games/game-prime.js
@@ -0,0 +1,37 @@
+import readlineSync from 'readline-sync';
+import { getRandomNumber, userName } from '../index.js';
+
+const isPrimeNumber = (number) => {
+  if (number < 2) return false;
+  if (number > 1) {
+      for (let i = 2; i < number; i += 1) {
+          if (number % i === 0) return false;
+      };
+  };
+  return true;
+};
+
+export const gamePrime = (rounds = 3) => {
+  const primeRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  console.log(primeRule);
+
+  let answers = 0;
+  while (answers < rounds) {
+    const gameNumber = getRandomNumber(100);
+    console.log(`Question: ${gameNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const correctAnswer = isPrimeNumber(gameNumber) ? 'yes' : 'no';
+  
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      answers += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer :(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+      break;
+    }
+  
+    if (answers === 3) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
+};
