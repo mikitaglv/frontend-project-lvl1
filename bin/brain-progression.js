512f3620e289e676a09b57735916fbb644521870
Nikita Gulyaev <mikita.glv@gmail.com>
2022-12-09 15:07:48 +0300

    Moved the game logic to a separate module

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,37 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greeting from '../src/cli.js';
+import { gameProgression } from "../src/games/game-progression.js";
 
-const userName = greeting();
-console.log('What number is missing in the progression?');
-
-let answers = 0;
-while (answers < 3) {
-  const hiddenPosition = Math.floor(Math.random() * 10);
-  const progression = [];
-  const step = Math.floor(Math.random() * 10);
-  let currentNumber = Math.floor(Math.random() * 100);
-  for (let i = 0; i < 10; i += 1) {
-    if (i === hiddenPosition) {
-        progression.push('..');
-    } else {
-        progression.push(currentNumber);
-    }
-    currentNumber += step;
-  };
-
-  console.log(`Question: ${progression.join(' ')}`);
-  const correctAnswer = progression[hiddenPosition + 1] - step;
-  const userAnswer = Number(readlineSync.question('Your answer: '));
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-    answers += 1;
-  } else {
-    console.log(`'${userAnswer}' is wrong answer :(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-    break;
-  }
-
-  if (answers === 3) {
-    console.log(`Congratulations, ${userName}!`);
-  }
-}
+gameProgression();
---
+++ src/games/game-progression.js
@@ -0,0 +1,37 @@
+import readlineSync from 'readline-sync';
+import { getRandomNumber, userName } from '../index.js';
+
+export const gameProgression = (rounds = 3) => {
+  console.log('What number is missing in the progression?');
+
+  let answers = 0;
+  while (answers < rounds) {
+    const hiddenPosition = Math.floor(Math.random() * 10);
+    const progression = [];
+    const step = Math.floor(Math.random() * 10);
+    let currentNumber = Math.floor(Math.random() * 100);
+    for (let i = 0; i < 10; i += 1) {
+      if (i === hiddenPosition) {
+          progression.push('..');
+      } else {
+          progression.push(currentNumber);
+      }
+      currentNumber += step;
+    };
+
+    console.log(`Question: ${progression.join(' ')}`);
+    const correctAnswer = progression[hiddenPosition + 1] - step;
+    const userAnswer = Number(readlineSync.question('Your answer: '));
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      answers += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer :(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+      break;
+    }
+
+    if (answers === 3) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
+}
